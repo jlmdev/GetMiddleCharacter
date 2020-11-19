20d96c8e7c55b6db817cd620610d4016077ae263
Josh Mann <jlmann72@gmail.com>
2020-11-19 10:19:30 -0500

    Passed. Had to redo a lot. Avoided Index out of bounds by putting if at beginning to return string without calculating

--- Program.cs
+++ Program.cs
@@ -2,11 +2,35 @@
 
 namespace GetMiddleCharacter
 {
-  class Program
-  {
-    static void Main(string[] args)
+    class Program
     {
-      Console.WriteLine("Welcome to C#");
+        public static string GetMiddle(string s)
+        {
+            if (s.Length == 1)
+            {
+                return s;
+            }
+            decimal half = s.Length / 2;
+            var middle = Math.Floor(half);
+            char middleCharacter = s[Decimal.ToInt32(middle) - 1];
+            char middleSecond = s[Decimal.ToInt32(middle)];
+            if (s.Length % 2 == 0)
+            {
+
+                return string.Concat(middleCharacter, middleSecond);
+            }
+            else
+            {
+                return middleSecond.ToString();
+            }
+        }
+        static void Main(string[] args)
+        {
+            Console.WriteLine(GetMiddle("test"));
+            Console.WriteLine(GetMiddle("testing"));
+            Console.WriteLine(GetMiddle("middle"));
+            Console.WriteLine(GetMiddle("A"));
+
+        }
     }
-  }
 }
